80796e8861cfa7d6e4329c73b2efa0835a8b462e
Suwon Son <son50myth@gmail.com>
2024-11-04 18:51:40 +0900

    second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.printf("Hello World!");
+
+        System.out.printf("\nMy name is SuWon Son");
     }
 }
